00b03a71656d4250ae887143221b3a1cd0999955
Furilo19 <fabian@juwei.de>
2025-02-22 19:47:46 +0100

    Fix

--- nixos/configuration-old.nix
+++ nixos/configuration-old.nix
@@ -4,8 +4,6 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      
-      <home-manager/nixos>
     ];
     
   # ============================================== #
@@ -98,21 +96,6 @@
     #  thunderbird
     ];
   };
-  
-  
-  # ============================================== #
-  # Home Manager
-  # ============================================== #
-    
-  home-manager.users.fabian = { pkgs, ... }: {
-    home.packages = [ pkgs.atool pkgs.httpie ];
-    programs.bash.enable = true;
-
-    # The state version is required and should stay at the version you
-    # originally installed.
-    home.stateVersion = "24.11";
-  };
-  home-manager.useGlobalPkgs = true;
     
   # ============================================== #
   # Graphics / Nvidia
