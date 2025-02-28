2826865afd5a24cb4cd7cc31a6950c8f5679b35b
Furilo19 <fabian@juwei.de>
2025-02-28 15:32:41 +0100

    stean

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -155,11 +155,16 @@
   # ============================================== #
   # Steam / Gaming
   # ============================================== #
-    
+  
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+  
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
     gamescopeSession.enable = true;
   };
   
